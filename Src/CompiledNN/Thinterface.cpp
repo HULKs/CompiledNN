d80eb1178a6c2da4b24652aea257e8b2697b8cb9
Maximilian Schmidt <maximilian@schmidt.so>
2023-01-28 13:39:23 +0100

    Create constant and mutable tensor accessors

--- Src/CompiledNN/Thinterface.cpp
+++ Src/CompiledNN/Thinterface.cpp
@@ -11,22 +11,32 @@ CompiledNN::~CompiledNN() {
 
 void CompiledNN::compile(const char *filename) { core->compile(filename); }
 
-Tensor CompiledNN::input(std::size_t index) {
+Tensor CompiledNN::input(std::size_t index) const {
   Tensor tensor{core->input(index).data(), core->input(index).size(),
                 core->input(index).dims().data(),
                 core->input(index).dims().size()};
   return tensor;
 }
 
-Tensor CompiledNN::output(std::size_t index) {
+TensorMut CompiledNN::input_mut(std::size_t index) {
+  TensorMut tensor{core->input(index).data(), core->input(index).size(),
+                   core->input(index).dims().data(),
+                   core->input(index).dims().size()};
+  return tensor;
+}
+
+Tensor CompiledNN::output(std::size_t index) const {
   Tensor tensor{core->output(index).data(), core->output(index).size(),
                 core->output(index).dims().data(),
                 core->output(index).dims().size()};
   return tensor;
 }
 
-unsigned long CompiledNN::inputSize(unsigned long index) {
-  return core->input(index).size();
+TensorMut CompiledNN::output_mut(std::size_t index) {
+  TensorMut tensor{core->output(index).data(), core->output(index).size(),
+                   core->output(index).dims().data(),
+                   core->output(index).dims().size()};
+  return tensor;
 }
 
 void CompiledNN::apply() { return core->apply(); }
